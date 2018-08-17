03eed274f6ef7fcee215e97ac41ca8472a812671
Simon-Pierre Fortin <simonpierre.fortin@ttessier.ca>
2018-08-17 16:08:05 -0400

    update

--- cryptopals_set1_challenge3.py
+++ cryptopals_set1_challenge3.py
@@ -1,16 +1,17 @@
 
 # This solution is far from complete
 
+pass_key_high = "ETAOIN SHRDLU"
+pass_key_low = "etaoin shrdlu"
+
 string1 = (bin(int("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", 16))[2:])
-string2 = (bin(int(ord("A")))[2:])
-string3 = ""
-while len(string1) < len(string3):
-    string3 = string3 + string2
-    print(string3)
+string2 = (bin(int(ord("R")))[2:])
+if len(string2) != 8:
+    string2 = "0" + string2
 result = ""
 
-for k, i in enumerate(string1):
-    compared = int(i) ^ int(string3[k])
-    result += str(compared)
-
-print(hex(int(result, 2))[2:])
+# for k, i in enumerate(string1):
+#     compared = int(i) ^ int(string3[k])
+#     result += str(compared)
+#
+# print(hex(int(result, 2))[2:])
