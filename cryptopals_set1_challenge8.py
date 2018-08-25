6009187098c152cf53372932a2374f28c799fc11
Ninjak0 <simon.pierre.fortin@gmail.com>
2018-08-25 12:52:32 -0400

    Added solution to challenge 8

--- cryptopals_set1_challenge8.py
+++ cryptopals_set1_challenge8.py
@@ -1,5 +1,16 @@
-# with open('8.txt') as input_file:
-#     ciphertext = input_file.readlines()
-#
-# for line in ciphertext:
-#     print(line)
+with open('8.txt') as input_file:
+    ciphertext = input_file.readlines()
+
+def find_ECB(ciphertext):
+    for line in ciphertext:
+        chunks = [line[i:i+16] for i in range(0, len(line), 16)]
+        for chunk in chunks:
+            reps = 0
+            for chunker in chunks:
+                if chunk == chunker:
+                    reps += 1
+            if reps > 1:
+                return "".join(chunks)
+
+
+print(find_ECB(ciphertext))
